b8e4ec07085f6b694d36e56e88c9e9f72fad1de4
puck1 <yaodidu@gmail.com>
2020-01-25 14:01:00 +0800

    solve 2

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
 |  5   | [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring) |     M      |   1    |  *   |            |
 |  6   | [ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion) |     M      |   1    |  *   |            |
 |  7   | [ Reverse Integer](https://leetcode.com/problems/reverse-integer) |     E      |   1    |      |            |
-|  8   | [String to Integer (atoi)](https://leetcode.com/problems/string-to-integer-atoi) |     M      |   0    |      |            |
-|      |                                                              |            |        |      |            |
-
+|  8   | [String to Integer (atoi)](https://leetcode.com/problems/string-to-integer-atoi) |     M      |   1    |      |            |
+|  9   | [Palindrome Number](https://leetcode.com/problems/palindrome-number) |     E      |   1    |  *   |            |
+|  10  |                                                              |            |        |      |            |
---
+++ isPalindrome.go
@@ -0,0 +1,64 @@
+// time O(2n), space O(n)
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	digits := make([]int, 0, 10)		// let cap 10? 
+	for x != 0 {
+		d := x % 10
+		x /= 10
+		digits = append(digits, d)
+	}
+	start, end := 0, len(digits) - 1
+	for start < end {
+		if digits[start] != digits[end] {
+			return false
+		} else {
+			start++
+			end--
+		}
+	}
+	return true
+}
+
+// judge directly, time O(n), space O(1) *
+func isPalindrome(x int) bool {
+    if x < 0 {
+        return false
+    }
+  base := int(math.Pow10(int(math.Log10(float64(x)))))  
+    for ; base >= 10; base /= 100 {
+        if x/base != x%10 {
+            return false
+        }
+        x = x % base / 10
+    }
+    return true
+}
+
+// judge (reversed x) == x ? time O(n), space O(1)
+// if reversed x exceed max int , go wrong *
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	tmp, rx := x, 0
+	for tmp > 0 {
+		rx = rx * 10 + tmp % 10
+		tmp /= 10
+	}
+	return rx == x
+}
+
+// judge half length of x, time O(n), space O(1) *
+func isPalindrome(x int) bool {
+	if x < 0 || (x % 10 == 0 && x != 0) {
+		return false
+	}
+	revered := 0
+	for x > revered {
+		revered = revered * 10 + x % 10
+		x /= 10
+	}
+	return revered == x || x == revered / 10
+}
